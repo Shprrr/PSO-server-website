e50e66f86921f75223a377dc263968737a328a01
Maxime Talon <maximetalon@hotmail.com>
2024-04-24 18:33:29 -0400

    Normal rare drops

---
+++ PSO server website/JsonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PSOServerWebsite;
+
+public static partial class JsonExtensions
+{
+    private static readonly Regex s_regex = RegexHexadecimal();
+    public static string FixJson(this string json)
+    {
+        return s_regex.Replace(json, new MatchEvaluator((Match match) => $"\"{match}\""));
+    }
+
+    [GeneratedRegex("0x[A-Fa-f0-9]+")]
+    private static partial Regex RegexHexadecimal();
+}
